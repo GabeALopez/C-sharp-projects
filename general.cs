bae20cedd8543de647ff6f98fc0af343e1f0e92f
GabeALopez <109094200+GabeALopez@users.noreply.github.com>
2023-11-06 17:52:34 -0500

    Finished attempting to use loop

--- general.cs
+++ general.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+/* swap values
 Console.WriteLine("Input the first number: ");
 int num1 = Int32.Parse(Console.ReadLine());
 Console.WriteLine("Input the second number: ");
@@ -10,3 +12,13 @@ num2 = num3;
 
 Console.WriteLine("First number: " + num1.ToString());
 Console.WriteLine("Second number: " + num2.ToString());
+*/
+
+int[] arr = {1, 2, 3, 4, 5};
+
+foreach(int x in arr)
+{
+    
+    Console.WriteLine($"{x}");
+
+}
